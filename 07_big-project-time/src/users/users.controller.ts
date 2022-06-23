8db8a728bf9df44ffb148f627eaba04459d07f3d
ueirt365 <nguyendinhtrieu365@gmail.com>
2022-06-23 22:04:15 +0700

    11 - Config session for signin, signup & implement getting my account api

--- 07_big-project-time/src/users/users.controller.ts
+++ 07_big-project-time/src/users/users.controller.ts
@@ -7,6 +7,7 @@ import {
   Patch,
   Post,
   Query,
+  Session,
 } from '@nestjs/common';
 import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { AuthService } from './auth.service';
@@ -23,14 +24,23 @@ export class UsersController {
     private authService: AuthService,
   ) {}
 
+  @Get('/me')
+  getMyAccount(@Session() session: any) {
+    return this.userService.findOne(session.userId);
+  }
+
   @Post('/signup')
-  createUser(@Body() body: CreateUserDto) {
-    this.authService.signup(body.email, body.password);
+  async createUser(@Body() body: CreateUserDto, @Session() session: any) {
+    const user = await this.authService.signup(body.email, body.password);
+    session.userId = user.id;
+    return user;
   }
 
   @Post('/signin')
-  signin(@Body() body: CreateUserDto) {
-    return this.authService.signin(body.email, body.password);
+  async signin(@Body() body: CreateUserDto, @Session() session: any) {
+    const user = await this.authService.signin(body.email, body.password);
+    session.userId = user.id;
+    return user;
   }
 
   @Get('/:id')
