13ea5c7e6b8096fb57f849a044c1ef79a18a5ea9
ueirt365 <nguyendinhtrieu365@gmail.com>
2022-07-25 07:38:20 +0700

    11 - Implement Decorator & Interceptor

--- 07_big-project-time/src/users/users.controller.ts
+++ 07_big-project-time/src/users/users.controller.ts
@@ -8,25 +8,35 @@ import {
   Post,
   Query,
   Session,
+  UseInterceptors
 } from '@nestjs/common';
 import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { AuthService } from './auth.service';
+import { CurrentUser } from './decorators/current-user.decorator';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { UserDto } from './dto/user.dto';
+import { CurrentUserInterceptor } from './interceptors/current-user.interceptor';
+import { UserEntity } from './user.entity';
 import { UsersService } from './users.service';
 
 @Controller('auth')
 @Serialize(UserDto)
+@UseInterceptors(CurrentUserInterceptor)
 export class UsersController {
   constructor(
     private userService: UsersService,
     private authService: AuthService,
   ) {}
 
+  // @Get('/me')
+  // getMyAccount(@Session() session: any) {
+  //   return this.userService.findOne(session.userId);
+  // }
+
   @Get('/me')
-  getMyAccount(@Session() session: any) {
-    return this.userService.findOne(session.userId);
+  getMyAccount(@CurrentUser() user: UserEntity) {
+    return user;
   }
 
   @Post('/signout')
--- 07_big-project-time/src/users/users.module.ts
+++ 07_big-project-time/src/users/users.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthService } from './auth.service';
+import { CurrentUserInterceptor } from './interceptors/current-user.interceptor';
 import { UserEntity } from './user.entity';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([UserEntity])],
-  providers: [UsersService, AuthService],
+  providers: [UsersService, AuthService, CurrentUserInterceptor],
   controllers: [UsersController],
 })
 export class UsersModule {}
