20877a907a368c2de00bca777e1194a0fd5c2271
ueirt365 <nguyendinhtrieu365@gmail.com>
2022-07-27 22:57:25 +0700

    14 - Managing App Configuration

--- 07_big-project-time/src/app.module.ts
+++ 07_big-project-time/src/app.module.ts
@@ -6,15 +6,31 @@ import { ReportsModule } from './reports/reports.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from './users/user.entity';
 import { ReportEntity } from './reports/report.entity';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: 'db.sqlite',
-      entities: [UserEntity, ReportEntity],
-      synchronize: true,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`,
     }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        return {
+          type: 'sqlite',
+          database: configService.get<string>('DB_NAME'),
+          entities: [UserEntity, ReportEntity],
+          synchronize: true,
+        };
+      },
+    }),
+    // TypeOrmModule.forRoot({
+    //   type: 'sqlite',
+    //   database: 'db.sqlite',
+    //   entities: [UserEntity, ReportEntity],
+    //   synchronize: true,
+    // }),
     UsersModule,
     ReportsModule,
   ],
