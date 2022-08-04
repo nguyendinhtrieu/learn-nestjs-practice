27403f4527b4dc353de72b389a6b55549c699338
ueirt365 <nguyendinhtrieu365@gmail.com>
2022-08-04 21:22:35 +0700

    15 - Implement API create report

--- 07_big-project-time/src/reports/reports.service.ts
+++ 07_big-project-time/src/reports/reports.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateReportDto } from './dtos/create-report-dto';
+import { ReportEntity } from './report.entity';
 
 @Injectable()
-export class ReportsService {}
+export class ReportsService {
+  constructor(
+    @InjectRepository(ReportEntity) private repo: Repository<ReportEntity>,
+  ) {}
+
+  create(reportDto: CreateReportDto) {
+    const report = this.repo.create(reportDto);
+    return this.repo.save(report);
+  }
+}
