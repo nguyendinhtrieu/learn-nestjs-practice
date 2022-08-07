15341d86b91b7be372914c34a81d70fd6f4c9e72
ueirt365 <nguyendinhtrieu365@gmail.com>
2022-08-07 22:10:21 +0700

    16 - Update approval report

--- 07_big-project-time/src/reports/reports.controller.ts
+++ 07_big-project-time/src/reports/reports.controller.ts
@@ -1,8 +1,16 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Param,
+  Patch,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { AuthGuard } from 'src/guards/auth.guard';
 import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { CurrentUser } from 'src/users/decorators/current-user.decorator';
 import { UserEntity } from 'src/users/user.entity';
+import { ApproveRequestDto } from './dtos/approve-request.dto';
 import { CreateReportDto } from './dtos/create-report-dto';
 import { ReportDto } from './dtos/report.dto';
 import { ReportsService } from './reports.service';
@@ -16,5 +24,11 @@ export class ReportsController {
   @Serialize(ReportDto)
   createReport(@Body() body: CreateReportDto, @CurrentUser() user: UserEntity) {
     return this.reportService.create(body, user);
+  }
+
+  @Patch('/:id')
+  @UseGuards(AuthGuard)
+  approveRequest(@Param('id') id: number, @Body() body: ApproveRequestDto) {
+    return this.reportService.approveRequest(id, body.approved);
   }
 }
--- 07_big-project-time/src/reports/reports.service.ts
+++ 07_big-project-time/src/reports/reports.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserEntity } from 'src/users/user.entity';
 import { Repository } from 'typeorm';
@@ -14,6 +14,16 @@ export class ReportsService {
   create(reportDto: CreateReportDto, user: UserEntity) {
     const report = this.repo.create(reportDto);
     report.user = user;
+    return this.repo.save(report);
+  }
+
+  async approveRequest(id: number, approved: boolean) {
+    const report = await this.repo.findOneBy({ id });
+    if (!report) {
+      throw new NotFoundException('report not found');
+    }
+    report.approved = approved;
+
     return this.repo.save(report);
   }
 }
